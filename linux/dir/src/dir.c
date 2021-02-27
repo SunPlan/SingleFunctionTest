b5268d2a23f314bb9fd3d9880db333231c183a1a
sunjunhao <sunjunhao@kylinos.cn>
2021-02-27 09:48:36 +0800

    docs(doc): add markDown test file
    Description: 增加markdown文档
    
    Log: 增加markdown文档

---
+++ linux/dir/src/dir.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include<sys/stat.h>
+#include<sys/types.h>
+
+#define TRUE    1
+#define FALSE   0
+
+
+
+char CreateDir(char *pDirName, char *pErrorMsg){
+
+    
+    // mkdir("head/follow".0777);
+    mkdir("head/follow/end",0777);
+
+
+    return TRUE;
+}
+
+
